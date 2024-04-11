fa8a9cdcf9d59412c3ed96b5f59d917e1f8fee8c
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-04-11 13:44:52 +0200

    config modified

---
+++ chatbot/src/components/BotAvatar/BotAvatar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./BotAvatar.css"
+
+const BotAvatar = () => {
+    return(
+        <div className="bot-avatar">Bot</div>
+    )
+}
+ 
+export default BotAvatar;
--- chatbot/src/config.js
+++ chatbot/src/config.js
@@ -1,8 +1,16 @@
 // Config starter code
+//aqui se pueden cambiar las caracteristicas esteticas
+import React from "react";
 import { createChatBotMessage } from "react-chatbot-kit";
+
+import BotAvatar from "./components/BotAvatar/BotAvatar";
 
 const config = {
   initialMessages: [createChatBotMessage(`Hello world`)],
-};
+  botName: "EmpleaBot",
+  customComponents:{
+    botAvatar: (props) => <BotAvatar {...props} />
+  }
+}
 
 export default config;
