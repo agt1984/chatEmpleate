03bf61899a72a7e42840450f7cb5e09c7cab50b3
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-04-15 14:53:45 +0200

    template created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import React from "react";
+import Chatbot from "react-chatbot-kit";
+import config from "./config.js";
+import MessageParser from "./MessageParser.jsx";
+import ActionProvider from "./ActionProvider.jsx";
+
+import "react-chatbot-kit/build/main.css";
 
 function App() {
   return (
     <div className="App">
-      divvvv
+      <Chatbot
+        config={config}
+        messageParser={MessageParser}
+        actionProvider={ActionProvider}
+      />
     </div>
   );
 }
